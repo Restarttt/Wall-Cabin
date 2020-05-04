183d18c14432aa68e026e75a10ee470995996c20
Your Name <you@example.com>
2020-05-04 17:21:27 +0800

    wall

--- wall cabin/miniprogram/pages/index/index.js
+++ wall cabin/miniprogram/pages/index/index.js
@@ -8,50 +8,75 @@ Page({
     indicatorActiveColor: "#FFF",
     indicatorColor: "rbga(251,255,242)",
     circular: true,
-    interval: "4000",
+    interval: "3000",
     duration: "1000",
     mode: 'aspectFill',
     list: [
-      "../../images/DeltaJunction_ZH-CN9901755694_1920x1080.jpg",
-      "../../images/FinWhale_ZH-CN9010064973_1920x1080.jpg",
-      "../../images/FreshSalt_ZH-CN12818759319_1920x1080.jpg",
-      "../../images/HallwylfjelletSunset_ZH-CN9300910376_1920x1080.jpg",
-      "../../images/midmoon_ZH-CN4973736313_1920x1080.jpg",
-      "../../images/DeltaJunction_ZH-CN9901755694_1920x1080.jpg",
+      "../../images/larger/FinWhale_ZH-CN9010064973_1920x1080.jpg",
+      "../../images/larger/FreshSalt_ZH-CN12818759319_1920x1080.jpg",
+      "../../images/larger/HallwylfjelletSunset_ZH-CN9300910376_1920x1080.jpg",
+      "../../images/larger/ThamesEstuaryNASA_EN-AU14188404911_1920x1080.jpg",
+      "../../images/larger/midmoon_ZH-CN4973736313_1920x1080.jpg",
+      "../../images/larger/DeltaJunction_ZH-CN9901755694_1920x1080.jpg",
+      "../../images/larger/SmogenSweden_ZH-CN0457682922_1920x1080.jpg",
+      "../../images/larger/EiffelBelow_ZH-CN5149009072_1920x1080.jpg",
     ],
     icon_list: [{
-        img: "../../images/hot.png",
-        text: "hot"
+        img: "../../images/hot1.png",
+        text: "专栏"
       },
       {
-        img: "../../images/hot.png",
-        text: "hot"
+        img: "../../images/hot1.png",
+        text: "排行"
       },
       {
-        img: "../../images/hot.png",
-        text: "hot"
+        img: "../../images/hot1.png",
+        text: "最近"
       },
       {
-        img: "../../images/hot.png",
-        text: "hot"
+        img: "../../images/hot1.png",
+        text: "热门"
       },
       {
-        img: "../../images/hot.png",
-        text: "hot"
+        img: "../../images/hot1.png",
+        text: "随机"
       },
       {
-        img: "../../images/hot.png",
-        text: "hot"
+        img: "../../images/hot1.png",
+        text: "4K"
       }, {
-        img: "../../images/hot.png",
-        text: "hot"
+        img: "../../images/hot1.png",
+        text: "PC"
       },
       {
-        img: "../../images/hot.png",
-        text: "hot"
+        img: "../../images/hot1.png",
+        text: "更多"
       },
 
     ]
   },
+  scenery_data: [{
+    back: "../../images/scenery/back1.png",
+    dot: '../../images/scenery/dot.png',
+    list: [{
+        img: '../../images/scenery/20200504164826.jpg'
+      },
+      {
+        img: '../../images/scenery/20200504164826.jpg'
+      },
+      {
+        img: '../../images/scenery/20200504164826.jpg'
+      },
+      {
+        img: '../../images/scenery/20200504164826.jpg'
+      }, {
+        img: '../../images/scenery/20200504164826.jpg'
+      },
+      {
+        img: '../../images/scenery/20200504164826.jpg'
+      }
+
+    ]
+  }]
 
 })
