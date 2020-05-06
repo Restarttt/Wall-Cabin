18560b35723a88494d060ecb5098104084b9aedf
Your Name <you@example.com>
2020-05-06 22:28:31 +0800

    wall

--- wall cabin/miniprogram/pages/lately/lately.js
+++ wall cabin/miniprogram/pages/lately/lately.js
@@ -5,6 +5,20 @@ Page({
    * 页面的初始数据
    */
   data: {
+    list: [{
+      img: "../../images/scenery/20200504164826.jpg"
+    }, {
+      img: "../../images/scenery/20200504164826.jpg"
+    }, {
+      img: "../../images/scenery/20200504164826.jpg"
+    }, {
+      img: "../../images/scenery/20200504164826.jpg"
+    }, {
+      img: "../../images/scenery/20200504164826.jpg"
+    },{img: "../../images/scenery/20200504164826.jpg"
+  }
+
+  ]
 
   },
 
