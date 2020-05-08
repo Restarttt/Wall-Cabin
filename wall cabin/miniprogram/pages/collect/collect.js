d222e003995591c9ab1b89421f1353c7f578d2c8
Your Name <you@example.com>
2020-05-08 22:20:15 +0800

    wall

--- wall cabin/miniprogram/pages/collect/collect.js
+++ wall cabin/miniprogram/pages/collect/collect.js
@@ -6,20 +6,22 @@ Page({
    */
   data: {
     list: [{
-        img: "../../images/scenery/20200504164826.jpg"
-      }, {
-        img: "../../images/scenery/20200504164826.jpg"
-      }, {
-        img: "../../images/scenery/20200504164826.jpg"
-      }, {
-        img: "../../images/scenery/20200504164826.jpg"
-      }, {
-        img: "../../images/scenery/20200504164826.jpg"
-      }
-    ]
+      img: "../../images/scenery/20200504164826.jpg"
+    }, {
+      img: "../../images/scenery/20200504164826.jpg"
+    }, {
+      img: "../../images/scenery/20200504164826.jpg"
+    }, {
+      img: "../../images/scenery/20200504164826.jpg"
+    }, {
+      img: "../../images/scenery/20200504164826.jpg"
+    }]
   },
-  delete(){
-    
+  delete() {
+    console.log(this.list)
+    this.list.splice(index, 1)
+    this.setData({
+      list: this.list.splice(index, 1)
+    })
   }
-
 })
